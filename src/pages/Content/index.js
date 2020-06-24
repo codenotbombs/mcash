9cf75a0efc40ee680e8273eafd5de5916c5122f3
Shagoy <pat.wolleb@gmail.com>
2020-06-24 22:59:27 +0200

    POC adding a button to SC

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,4 +3,15 @@ import { printLine } from './modules/print';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+const hero = document.getElementsByClassName('fullHero__foreground fullListenHero__foreground')[0]
+
+console.log(hero)
+
+
+var buyButton = document.createElement('button');
+hero.appendChild(buyButton);
+buyButton.innerText = 'Buy';
+
+buyButton.addEventListener('click', (e) => {
+    console.log('Buy: ', document.location.href)
+})
